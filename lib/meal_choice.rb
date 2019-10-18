92f5e96199d2c17f67fdcd42bf7891b7ec52cb05
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-18 17:13:52 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,7 @@
 def meal_choice
-  puts "What a nurtritious meal!"
+  puts #{veg1}
+  puts #{veg2}
+  puts #{meat}
+  "What a nurtritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
